21e586db74432b59f75da2b253a2cfd88db55a78
KKIMMINSEOKK <k_ms3164@naver.com>
2025-05-07 12:20:53 +0900

    script 수정

--- script_create.py
+++ script_create.py
@@ -2,18 +2,23 @@ import os
 dir = "./datasets/real/"
 
 # read all the files in the directory
-file  = os.listdir(dir)
+files  = os.listdir(dir)
 # we need to remove .DS_Store and MAG file 
 # file.remove('.DS_Store')
 # file.remove('MAG')
 
-file = ["contact"]
+# files = ["contact", "congress"]
+
 
 # write the output to a file
 with open("run.sh", "w") as f:
-    for file in file:
+    for file in files:
         for k in range(10,60,10):
-            for s in range(0,10):
-                for c in range(1,3):
-                    print(f"python3 main.py --network {dir}{file}/network.hyp --algorithm ks --k {k} --s {round(s * 0.1, 1)} --c {c} &&")
-                    f.write(f"python3 main.py --network {dir}{file}/network.hyp --algorithm ks --k {k} --s {round(s * 0.1, 1)} --c {c} &&\n")
+            for s in range(1,10):
+                for c in range(1, 4):
+                    print(f"python3 main.py --network {dir}{file}/network.hyp --algorithm ks --k {k} --s {round(s * 0.5, 1)} --c {round(c * 0.5, 1)} &&")
+                    f.write(f"python3 main.py --network {dir}{file}/network.hyp --algorithm ks --k {k} --s {round(s * 0.5, 1)} --c {round(c * 0.5, 1)} &&\n")
+
+with open("run.sh", "rb+") as f:
+    f.seek(-4, 2)
+    f.truncate()
