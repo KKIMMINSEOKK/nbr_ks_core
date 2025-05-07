90d89152c259a34eaf20e865e97fba0f2eec8e4c
KKIMMINSEOKK <k_ms3164@naver.com>
2025-05-07 12:22:27 +0900

    script 수정

--- script_create.py
+++ script_create.py
@@ -16,8 +16,8 @@ with open("run.sh", "w") as f:
         for k in range(10,60,10):
             for s in range(1,10):
                 for c in range(1, 4):
-                    print(f"python3 main.py --network {dir}{file}/network.hyp --algorithm ks --k {k} --s {round(s * 0.5, 1)} --c {round(c * 0.5, 1)} &&")
-                    f.write(f"python3 main.py --network {dir}{file}/network.hyp --algorithm ks --k {k} --s {round(s * 0.5, 1)} --c {round(c * 0.5, 1)} &&\n")
+                    print(f"python main.py --network {dir}{file}/network.hyp --algorithm ks --k {k} --s {round(s * 0.5, 1)} --c {round(c * 0.5, 1)} &&")
+                    f.write(f"python main.py --network {dir}{file}/network.hyp --algorithm ks --k {k} --s {round(s * 0.5, 1)} --c {round(c * 0.5, 1)} &&\n")
 
 with open("run.sh", "rb+") as f:
     f.seek(-4, 2)
